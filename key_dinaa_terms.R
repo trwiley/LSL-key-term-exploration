1f09c6adf421ed90032f15387e4994cf20b11a72
trwiley <taylor.r.wiley@gmail.com>
2018-02-02 19:03:07 -0500

    Generated a frequency table of words.

--- key_dinaa_terms.R
+++ key_dinaa_terms.R
@@ -1,6 +1,10 @@
 #key_dinaa_terms.R
-#Purpose: generate a wordcloud in order to find key terms within data from DINAA LSL.
-#Dependencies: wordcloud2
+#Purpose: generate a frequency table of all of the words in titles of referenced literature in DINAA LSL.
 
 referenced <- lsl_data$`Is Referenced By`
 
+referenced_coll <- paste(referenced, collapse = " ")
+
+ref_words <- strsplit(referenced_coll, split = "[ ]")
+
+ref_table <- table(ref_words)
